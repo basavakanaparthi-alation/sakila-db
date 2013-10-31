8cd09ee4744a8a5d7e84d7620a88165847efafab
gobigondata <chandhana@BHARATHIRAJAs-MacBook-Pro.local>
2013-10-31 08:08:37 -0700

    fixed wrong DDLs

--- hive_ddl.sql
+++ hive_ddl.sql
@@ -1,6 +1,6 @@
 create database sakila;
 
-CREATE  EXTERNAL TABLE sakila.actor (
+CREATE  EXTERNAL TABLE actor (
   actor_id int,
   first_name string,
   last_name string,
@@ -25,8 +25,7 @@ ROW FORMAT DELIMITED
   LINES TERMINATED BY '\n'
   STORED AS TEXTFILE
   LOCATION '/ext/sakila/address';
-  
-  
+
 CREATE  external TABLE sakila.category(
   category_id tinyint,
   name string,
@@ -37,86 +36,74 @@ CREATE  external TABLE sakila.category(
   LOCATION '/ext/sakila/category';
 
 CREATE external TABLE sakila.city(
-  address_id int,
-  address string,
-  address2 string,
-  district string,
   city_id int,
-  postal_code string,
-  phone string,
+  city string,
+  country_id int,
   last_update string)
   ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
   LINES TERMINATED BY '\n'
   STORED AS TEXTFILE
   LOCATION '/ext/sakila/city';
 
+
 CREATE external TABLE sakila.country(
-  address_id int,
-  address string,
-  address2 string,
-  district string,
-  city_id int,
-  postal_code string,
-  phone string,
+ country_id int,
+  country string,
   last_update string)
   ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
   LINES TERMINATED BY '\n'
   STORED AS TEXTFILE
   LOCATION '/ext/sakila/country';
 
-  
 CREATE external TABLE sakila.customer(
+  customer_id int,
+  store_id tinyint,
+  first_name string,
+  last_name string,
+  email string,
   address_id int,
-  address string,
-  address2 string,
-  district string,
-  city_id int,
-  postal_code string,
-  phone string,
+  active boolean,
+  create_date string,
   last_update string)
   ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
   LINES TERMINATED BY '\n'
   STORED AS TEXTFILE
   LOCATION '/ext/sakila/customer';
 
-  
+
 CREATE external TABLE sakila.film(
-  address_id int,
-  address string,
-  address2 string,
-  district string,
-  city_id int,
-  postal_code string,
-  phone string,
+  film_id int,
+  title string,
+  description string,
+  release_year string,
+  language_id tinyint,
+  original_language_id tinyint,
+  rental_duration tinyint,
+  rental_rate double,
+  length int,
+  replacement_cost double,
+  rating string,
+  special_features string,
   last_update string)
   ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
   LINES TERMINATED BY '\n'
   STORED AS TEXTFILE
   LOCATION '/ext/sakila/film';
 
-  
+
 CREATE external TABLE sakila.film_actor(
-  address_id int,
-  address string,
-  address2 string,
-  district string,
-  city_id int,
-  postal_code string,
-  phone string,
+ actor_id int,
+  film_id int,
   last_update string)
   ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
   LINES TERMINATED BY '\n'
   STORED AS TEXTFILE
   LOCATION '/ext/sakila/film_actor';
 
+
 CREATE external TABLE sakila.film_category(
-  address_id int,
-  address string,
-  address2 string,
-  district string,
-  city_id int,
-  postal_code string,
-  phone string,
+ film_id int,
+  category_id tinyint,
   last_update string)
   ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
   LINES TERMINATED BY '\n'
@@ -124,27 +111,19 @@ CREATE external TABLE sakila.film_category(
   LOCATION '/ext/sakila/film_category';
 
 CREATE external TABLE sakila.film_text(
-  address_id int,
-  address string,
-  address2 string,
-  district string,
-  city_id int,
-  postal_code string,
-  phone string,
-  last_update string)
+ film_id int,
+  title string,
+  description string)
   ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
   LINES TERMINATED BY '\n'
   STORED AS TEXTFILE
   LOCATION '/ext/sakila/film_text';
 
+
 CREATE external TABLE sakila.inventory(
-  address_id int,
-  address string,
-  address2 string,
-  district string,
-  city_id int,
-  postal_code string,
-  phone string,
+ inventory_id bigint,
+  film_id int,
+  store_id tinyint,
   last_update string)
   ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
   LINES TERMINATED BY '\n'
@@ -152,27 +131,22 @@ CREATE external TABLE sakila.inventory(
   LOCATION '/ext/sakila/inventory';
 
 CREATE external TABLE sakila.language(
-  address_id int,
-  address string,
-  address2 string,
-  district string,
-  city_id int,
-  postal_code string,
-  phone string,
+language_id tinyint,
+  name string,
   last_update string)
   ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
   LINES TERMINATED BY '\n'
   STORED AS TEXTFILE
   LOCATION '/ext/sakila/language';
 
+
 CREATE external TABLE sakila.payment(
-  address_id int,
-  address string,
-  address2 string,
-  district string,
-  city_id int,
-  postal_code string,
-  phone string,
+ payment_id int,
+  customer_id int,
+  staff_id tinyint,
+  rental_id int,
+  amount double,
+  payment_date string,
   last_update string)
   ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
   LINES TERMINATED BY '\n'
@@ -180,43 +154,26 @@ CREATE external TABLE sakila.payment(
   LOCATION '/ext/sakila/payment';
 
 CREATE external TABLE sakila.rental(
-  address_id int,
-  address string,
-  address2 string,
-  district string,
-  city_id int,
-  postal_code string,
-  phone string,
+ rental_id int,
+  rental_date string,
+  inventory_id bigint,
+  customer_id int,
+  return_date string,
+  staff_id tinyint,
   last_update string)
   ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
   LINES TERMINATED BY '\n'
   STORED AS TEXTFILE
   LOCATION '/ext/sakila/rental';
 
-CREATE external TABLE sakila.staff(
-  address_id int,
-  address string,
-  address2 string,
-  district string,
-  city_id int,
-  postal_code string,
-  phone string,
-  last_update string)
-  ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
-  LINES TERMINATED BY '\n'
-  STORED AS TEXTFILE
-  LOCATION '/ext/sakila/staff';
 
 CREATE external TABLE sakila.store(
+ store_id tinyint,
+  manager_staff_id tinyint,
   address_id int,
-  address string,
-  address2 string,
-  district string,
-  city_id int,
-  postal_code string,
-  phone string,
   last_update string)
   ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
   LINES TERMINATED BY '\n'
   STORED AS TEXTFILE
   LOCATION '/ext/sakila/store';
+
